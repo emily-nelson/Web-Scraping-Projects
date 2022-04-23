781dffa2b3f07cb31ff2d89ac1de8770c32e331e
emily-nelson <94139746+emily-nelson@users.noreply.github.com>
2022-04-23 16:25:30 +0100

    first commit

---
+++ mapit.py
@@ -0,0 +1,18 @@
+import webbrowser, sys,# pyperclip
+
+# sys.argv stores a list of a programs's filename and command line arguments
+# If its length is greater than 1, then a command line argument has been provided
+# We need to use the join() method as sys.argv is a list of strings and [1:] to chop off 1st elemetn in the array which is the filename
+address = ""
+
+if len(sys.argv) > 1:
+    # Get address fro command line
+    address = ' '.join(sys.argv[1:]) 
+#else:
+    #address = pyperclip.paste()
+
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
+
+
+
